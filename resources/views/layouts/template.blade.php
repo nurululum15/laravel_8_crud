54c66dab0c031f6427aa88d59d311127e1befe97
Nurul Izzatil Ulum <76694068+nurululum15@users.noreply.github.com>
2021-10-30 23:59:32 +0700

    crud

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,14 +4,18 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | @yield('title')</title>
+  <title>My Projects | @yield('title')</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('template')}}/plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{asset('template')}}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{asset('template')}}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('template')}}/dist/css/adminlte.min.css">
+  
   
 </head>
 <body class="hold-transition sidebar-mini">
@@ -19,57 +23,56 @@
 <div class="wrapper">
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-
-  <!-- Left navbar links -->
-  <ul class="navbar-nav">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-  </ul>
+    </ul>
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+    </ul>
 
-  <!-- Right navbar links -->
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-        <i class="fas fa-expand-arrows-alt"></i>
-      </a>
-    </li>
-  </ul>
-
-  <!-- User Account: style can be found in dropdown.less -->
-  <li class="navbar-nav">
+    <li class="navbar-nav">
       <a href="#" class="nav-link" data-toggle="dropdown">
       <i class="fas fa-user"></i>
-        <span class="hidden-xs">{{ Auth::user()->name }}</span>
+        <span class="hidden-xs"> {{ Auth::user()->name }}</span>
       </a>
       <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
         <span class="dropdown-item dropdown-header">My Profile</span>
-        <div class="dropdown-divider"></div>
-        <a href="favorites" class="dropdown-item">
-          <i class="fas fa-star mr-2"></i> Favorites
-          <span class="float-right text-muted text-sm">3 mins</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-          <i class="fas fa-print mr-2"></i> Print My Favorites
-          <span class="float-right text-muted text-sm">12 hours</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item dropdown-footer" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit(); >Logout</a>
+          <div class="dropdown-divider"></div>
+              <a href="favorites" class="dropdown-item">
+                <i class="fas fa-star mr-2"></i> Favorites
+                <span class="float-right text-muted text-sm">3 mins</span>
+              </a>
+          <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item">
+                <i class="fas fa-print mr-2"></i> Print My Favorites
+                <span class="float-right text-muted text-sm">12 hours</span>
+              </a>
+          <div class="dropdown-divider"></div>
+            <div class="dropdown-items dropdown-footer">
+              <form id="logout-form" action="{{ route('logout') }}" method="POST"> 
+                @csrf
+                <button type="submit" class="btn btn-default"> Logout </button>
+              </form>
+            </div>
       </div>
     </li>
-  </nav>
-  <!-- /.navbar -->
-
   </nav>
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('home')}}" class="brand-link">
+    <a href="../../index3.html" class="brand-link">
       <img src="{{asset('template')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">My Projects</span>
     </a>
 
     <!-- Sidebar -->
@@ -77,7 +80,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="info">
-          <a href="#" class="d-block">Hello {{ Auth::user()->name }} </a>
+          <a href="#" class="d-block">Hello  {{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -119,13 +122,6 @@
   </div>
   <!-- /.content-wrapper -->
 
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.1.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-
 </div>
 <!-- ./wrapper -->
 
@@ -137,5 +133,16 @@
 <script src="{{asset('template')}}/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset('template')}}/dist/js/demo.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="{{asset('template')}}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{asset('template')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{asset('template')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{asset('template')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+
+<script>
+  // $(document).ready(function(){
+    $('#table1').DataTable();
+  // });
+</script>
 </body>
 </html>
